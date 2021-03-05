a1a9ad33dd46366a5c8f27cd04dd132e81843121
Raphael Scaziti <rscaziti@gmail.com>
2021-03-05 13:45:14 -0300

    feat(categoria/controller): adicionado método GetById

--- petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,13 @@ public class CategoriaController
 	public ResponseEntity<List<CategoriaModel>> GetAll()
 	{
 		return ResponseEntity.ok(this.repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoriaModel> GetById(@PathVariable Long id)
+	{
+		return this.repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
