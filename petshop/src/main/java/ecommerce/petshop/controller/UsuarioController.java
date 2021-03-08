b7f920ed3edbfbe85d0bf75e514502bdb19c97a7
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 14:48:06 -0300

    feat(usuario/controller): adicionado método PUT

--- petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,8 +46,14 @@ public class UsuarioController
 	}
 	
 	@PostMapping
-	public ResponseEntity<UsuarioModel> Put(@RequestBody UsuarioModel usuario)
+	public ResponseEntity<UsuarioModel> Post(@RequestBody UsuarioModel usuario)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<UsuarioModel> Put(@RequestBody UsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(usuario));
 	}
 }
