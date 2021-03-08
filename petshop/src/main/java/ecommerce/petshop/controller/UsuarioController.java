b1df5f9bc8e618b0d94322c8a14dbc6a1bb578d1
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 14:35:12 -0300

    feat(usuario/controller): criado classe controller com método GetAll

---
+++ petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package ecommerce.petshop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ecommerce.petshop.model.UsuarioModel;
+import ecommerce.petshop.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController 
+{
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<UsuarioModel>> GetAll()
+	{
+		return ResponseEntity.ok(this.repository.findAll());
+	}
+}
