1c8faee428cd357b80bc14c50e8bd277d86bb26d
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 15:31:42 -0300

    feat(categoria/controller): adicionado método DELETE

--- petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,11 @@ public class CategoriaController
 	public ResponseEntity<CategoriaModel> Put(@RequestBody CategoriaModel categoria)
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void Delete(@PathVariable Long id)
+	{
+		this.repository.deleteById(id);
 	}
 }
