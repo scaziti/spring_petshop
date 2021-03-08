4bf18456829abda27eaf41ca837887915b0d465d
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 14:43:35 -0300

    feat(usuario/controller): adicionado o método POST

--- petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
@@ -3,10 +3,13 @@ package ecommerce.petshop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +42,11 @@ public class UsuarioController
 	public ResponseEntity<List<UsuarioModel>> GetByNome(@PathVariable String nome)
 	{
 		return ResponseEntity.ok(this.repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<UsuarioModel> Put(@RequestBody UsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(usuario));
 	}
 }
