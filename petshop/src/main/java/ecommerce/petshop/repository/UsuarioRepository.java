a0841c7796635e8bfe57e7c9614a0dc2199f2b1d
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 12:24:42 -0300

    feat(usuario/repository): adicionado interface de repository com método findAllByNome

---
+++ petshop/src/main/java/ecommerce/petshop/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package ecommerce.petshop.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ecommerce.petshop.model.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>
+{
+	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
+}
