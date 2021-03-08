72fb5089572c3dbdc92b191be5f82a698a227a50
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 15:30:28 -0300

    feat(categoria/controller): adicionado método PUT

--- petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,11 @@ public class CategoriaController
 	public ResponseEntity<CategoriaModel> Post(@RequestBody CategoriaModel categoria)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<CategoriaModel> Put(@RequestBody CategoriaModel categoria)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(categoria));
 	}
 }
