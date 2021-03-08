028405d3733f018f5e1d2728cd200d37ff64bc23
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 11:52:40 -0300

    feat(produto/model): adicionado atributos id, nome, preco, estoque e ativo

---
+++ petshop/src/main/java/ecommerce/petshop/model/ProdutoModel.java
@@ -0,0 +1,73 @@
+package ecommerce.petshop.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class ProdutoModel 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 3, max = 150)
+	private String nome;
+	
+	@NotNull
+	private Double preco;
+	
+	@NotNull
+	private Integer estoque;
+	
+	@NotNull
+	private Boolean ativo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
