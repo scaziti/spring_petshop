2d355364658bf948a434b40c88dce39f7283461e
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 15:28:08 -0300

    feat(categoria/controller): adicionado método POST

--- petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/CategoriaController.java
@@ -3,10 +3,13 @@ package ecommerce.petshop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +42,11 @@ public class CategoriaController
 	public ResponseEntity<List<CategoriaModel>> GetByNome(@PathVariable String nomeCategoria)
 	{
 		return ResponseEntity.ok(this.repository.findAllByNomeCategoriaContainingIgnoreCase(nomeCategoria));
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoriaModel> Post(@RequestBody CategoriaModel categoria)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(categoria));
 	}
 }
