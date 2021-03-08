ec96ae2c896d3e6c98172cff9252af28aa891dab
Raphael Scaziti <rscaziti@gmail.com>
2021-03-08 14:37:27 -0300

    feat(usuario/controller): adicionado método GetById

--- petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
+++ petshop/src/main/java/ecommerce/petshop/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,13 @@ public class UsuarioController
 	public ResponseEntity<List<UsuarioModel>> GetAll()
 	{
 		return ResponseEntity.ok(this.repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioModel> GetById(@PathVariable Long id)
+	{
+		return this.repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
